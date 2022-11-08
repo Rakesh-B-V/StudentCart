f8b740ed4e16f7f3b4e70bc7c8acbe8309c9f9d1
balgigourish7 <balgigourish7@gmail.com>
2022-11-08 11:11:01 +0000

    Unit Test Project for APIs

--- StudentCart.WebServices/Startup.cs
+++ StudentCart.WebServices/Startup.cs
@@ -75,7 +75,7 @@ namespace StudentCart.WebServices
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            loggerFactory.AddLog4Net();
+            //loggerFactory.AddLog4Net();
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
